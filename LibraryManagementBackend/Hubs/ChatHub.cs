3954959804d93ffe76e4bcaa8717de2ee4261f3d
shine102 <nguyenngotunglam2@gmail.com>
2023-07-24 23:34:48 +0700

    .

--- LibraryManagementBackend/Hubs/ChatHub.cs
+++ LibraryManagementBackend/Hubs/ChatHub.cs
@@ -3,27 +3,20 @@
 	using Microsoft.AspNetCore.SignalR;
 	public class ChatHub : Hub
 	{
-		public async Task SendMessage(string user, string message)
+
+		public async Task SendMessage(string user, string message, string chatId)
 		{
-			await Clients.Group(GetGroupName()).SendAsync("ReceiveMessage", user, message);
+			await Clients.Group(chatId).SendAsync("ReceiveMessage", user, message);
 		}
 
-		public async Task JoinPrivateChat(string userId)
+		public async Task JoinPrivateChat(string chatId)
 		{
-			await Groups.AddToGroupAsync(Context.ConnectionId, GetGroupName(userId));
+			await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
 		}
 
-		public async Task LeavePrivateChat(string userId)
+		public async Task LeavePrivateChat(string chatId)
 		{
-			await Groups.RemoveFromGroupAsync(Context.ConnectionId, GetGroupName(userId));
-		}
-
-		private string GetGroupName(string userId = "")
-		{
-			return "123";
-			var currentUserId = userId != "" ? userId : Context.UserIdentifier;
-			var otherUserId = Context.UserIdentifier == "User1" ? "User2" : "User1";
-			return $"{currentUserId}-{otherUserId}";
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
 		}
 	}
 }
