a30f8d2eeae0e171d634fd5f9e111d2899c6a8ee
Manh Nguyen <manhnb1911@gmail.com>
2023-07-24 23:45:59 +0700

    save message to db

--- LibraryManagementBackend/Hubs/ChatHub.cs
+++ LibraryManagementBackend/Hubs/ChatHub.cs
@@ -1,22 +1,36 @@
 ﻿namespace LibraryManagementBackend.Hubs
 {
-	using Microsoft.AspNetCore.SignalR;
-	public class ChatHub : Hub
-	{
+    using LibraryManagementBackend.Business.ChatService;
+    using Microsoft.AspNetCore.SignalR;
 
-		public async Task SendMessage(string userId, string partnerId, string message, string chatId)
-		{
-			await Clients.Group(chatId).SendAsync("ReceiveMessage", userId, message);
-		}
+    public class ChatHub : Hub
+    {
+        private readonly IChatService chatService;
 
-		public async Task JoinPrivateChat(string chatId)
-		{
-			await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
-		}
+        public ChatHub(IChatService chatService)
+        {
+            this.chatService = chatService;
+        }
 
-		public async Task LeavePrivateChat(string chatId)
-		{
-			await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
-		}
-	}
+        public async Task SendMessage(string userId, string partnerId, string message, string chatId)
+        {
+            await this.chatService.SendMessage(new()
+            {
+                sender_id   = int.Parse(userId),
+                receiver_id = int.Parse(partnerId),
+                content     = message,
+            });
+            await Clients.Group(chatId).SendAsync("ReceiveMessage", userId, message);
+        }
+
+        public async Task JoinPrivateChat(string chatId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
+        }
+
+        public async Task LeavePrivateChat(string chatId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
+        }
+    }
 }
